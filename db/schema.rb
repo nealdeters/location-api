918f448ab66e287d0175d79fa409a0a35388d7a3
nealdeters <nealdeters@gmail.com>
2016-02-23 21:20:30 -0600

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214154642) do
+ActiveRecord::Schema.define(version: 20160224022019) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "city",           limit: 255
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160214154642) do
     t.string   "country",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
