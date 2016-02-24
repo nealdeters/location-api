1ec297932b34eb422ae9344e3b56f9ce085edac9
nealdeters <nealdeters@gmail.com>
2016-02-23 19:03:06 -0600

    added versioning and namespacing for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/' => 'locations#index'
-  get '/locations' => 'locations#index'
-  post '/locations' => 'locations#create'
-  get '/locations/:id' => 'locations#show'
-  patch '/locations/:id' => 'locations#update'
+  namespace :api do
+    namespace :v1 do
+      get '/' => 'locations#index'
+      get '/locations' => 'locations#index'
+      post '/locations' => 'locations#create'
+      get '/locations/:id' => 'locations#show'
+      patch '/locations/:id' => 'locations#update'
 
-  delete '/locations/:id' => 'locations#destroy'
+      delete '/locations/:id' => 'locations#destroy'
+    end
+  end
 end
